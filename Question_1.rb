d325a76d8db25986f92ac5cb1fbde2e7b21a11a2
Travis <TravisMG11@gmail.com>
2018-06-19 11:26:09 -0400

    Flesh out Question_1

--- Question_1.rb
+++ Question_1.rb
@@ -1,3 +1,25 @@
 def sort_by_string(s, t)
+  letters = Hash.new(0)
+  s_arr = s.chars
+  t_arr = t.chars
+  sorted_str = ""
+  misc_lttrs = ""
+
+  s_arr.each do |lttr|
+    letters[lttr] += 1
+    misc_lttrs << lttr unless t_arr.include?(lttr)
+  end
+
+  t_arr.each do |lttr|
+    if letters.has_key?(lttr)
+      letters[lttr].times do
+        sorted_str << lttr
+      end
+    end
+  end
+
+  sorted_str << misc_lttrs
 
 end
+
+sort_by_string("program", "grapo")
